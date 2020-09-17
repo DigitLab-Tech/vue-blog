7b47487cd454ec8eb194d66be7bf6296349a8d03
Cedrick <cedrick.plante@gmail.com>
2020-09-17 11:07:41 -0400

    theme restructuration, fonts dinamique

---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+    <header>
+        <h1>{{menuData}}</h1>
+    </header>
+</template>
+
+<script>
+
+    export default {
+        name: 'Header',
+        props:{
+            logo:Object,
+            menuData:Object
+        }
+    }
+</script>
+
+<style scoped>
+</style>
