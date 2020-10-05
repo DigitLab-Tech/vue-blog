b89b00f1ab64047ff8e7a3aa822268804af83f85
Cedrick <cedrick.plante@gmail.com>
2020-10-05 17:26:16 -0400

    progress bar

--- src/main.js
+++ src/main.js
@@ -1,16 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 import AOS from 'aos';
+import NProgresse from 'nprogresse';
+import 'nprogresse/style.css';
 import './assets/css/theme.css';
 import './assets/css/framework.css';
 import './assets/css/font-rules.css';
 import 'aos/dist/aos.css';
 
 Vue.config.productionTip = false;
+AOS.init();
+NProgresse.start();
 
 new Vue({
   mounted(){
-    AOS.init();
+    NProgresse.done();
   },
   render: h => h(App),
 }).$mount('#app');
