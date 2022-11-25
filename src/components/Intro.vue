0259c8fc9c8f73e9f0f4d7398c1fe5288e1f7f91
Cedrick <cedrick@nabupro.com>
2022-11-25 18:58:47 -0500

    adding flower texture

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="intro-container grid">
         <div class="left-container row stretch vstretch">
+            <div class="flower-container" :style="flowerContainerCss"></div>
             <div class="column start vcenter">
                 <h1 v-html="section.getTextContent('slogan')" data-aos="fade-down-right" data-aos-delay="175"></h1>
                 <div class="to-service-wrapper" data-aos="fade-up" data-aos-delay="300">
@@ -17,7 +18,6 @@
                 </div>
             </div>
         </div>
-        <div class="flower-container" :style="flowerContainerCss"></div>
     </div>
 </template>
 
@@ -102,6 +102,7 @@
         background-position: center;
         z-index: -1;
         width: 45vw;
+        position: absolute;
     }
 
     h2{
